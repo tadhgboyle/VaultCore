563bcc22654904b5e3e12a412fb5ea734fc2bcd1
Aberdeener <tadhgsmboyle@gmail.com>
2019-11-24 20:50:48 -0800

    convert to maven, having issues...

--- src/net/vaultmc/vaultcore/commands/HelpCommand.java
+++ src/net/vaultmc/vaultcore/commands/HelpCommand.java
@@ -1,116 +1,109 @@
 package net.vaultmc.vaultcore.commands;
 
-import net.vaultmc.vaultcore.Permissions;
-import net.vaultmc.vaultcore.Utilities;
-import net.vaultmc.vaultutils.utils.commands.experimental.*;
-import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
-
 import java.util.Arrays;
 import java.util.Collections;
 
-@RootCommand(
-        literal = "help",
-        description = "Show the help menu."
-)
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+import net.vaultmc.vaultcore.Permissions;
+import net.vaultmc.vaultcore.Utilities;
+import net.vaultmc.vaultutils.utils.commands.experimental.ArgumentProvider;
+import net.vaultmc.vaultutils.utils.commands.experimental.CommandExecutor;
+import net.vaultmc.vaultutils.utils.commands.experimental.Permission;
+import net.vaultmc.vaultutils.utils.commands.experimental.PlayerOnly;
+import net.vaultmc.vaultutils.utils.commands.experimental.RootCommand;
+import net.vaultmc.vaultutils.utils.commands.experimental.SubCommand;
+
+@RootCommand(literal = "help", description = "Show the help menu.")
 @Permission(Permissions.BackCommand)
 @PlayerOnly
 
 public class HelpCommand extends CommandExecutor {
 
-    public HelpCommand() {
-        this.register("help", Collections.emptyList(), "VaultCore");
-        this.register("generalHelp", Arrays.asList(
-                Commands.literal("general")),
-                "VaultCore");
-        this.register("creativeHelp", Arrays.asList(
-                Commands.literal("creative")),
-                "VaultCore");
-        this.register("survivalHelp", Arrays.asList(
-                Commands.literal("survival")),
-                "VaultCore");
-        this.register("clansHelp", Arrays.asList(
-                Commands.literal("clans")),
-                "VaultCore");
-        this.register("skyblockHelp", Arrays.asList(
-                Commands.literal("skyblock")),
-                "VaultCore");
-    }
+	public HelpCommand() {
+		this.register("help", Collections.emptyList(), "VaultCore");
+		this.register("generalHelp", Arrays.asList(Commands.literal("general")), "VaultCore");
+		this.register("creativeHelp", Arrays.asList(Commands.literal("creative")), "VaultCore");
+		this.register("survivalHelp", Arrays.asList(Commands.literal("survival")), "VaultCore");
+		this.register("clansHelp", Arrays.asList(Commands.literal("clans")), "VaultCore");
+		this.register("skyblockHelp", Arrays.asList(Commands.literal("skyblock")), "VaultCore");
+	}
 
-    String string = Utilities.string;
-    String variable1 = Utilities.variable1;
-    String variable2 = Utilities.variable2;
+	String string = Utilities.string;
+	String variable1 = Utilities.variable1;
+	String variable2 = Utilities.variable2;
 
-    @SubCommand("help")
-    public void rootHelp(CommandSender sender, ArgumentProvider args) {
+	@SubCommand("help")
+	public void rootHelp(CommandSender sender, ArgumentProvider args) {
 
-        sender.sendMessage(variable2 + "--== [Help] ==--");
-        sender.sendMessage(" ");
-        sender.sendMessage(variable1 + "Usage: " + ChatColor.RED + "/help <option> [page]");
-        sender.sendMessage(variable1 + "Available Options:");
-        sender.spigot().sendMessage(
-                Utilities.hoverMaker("General", "Helpful commands to be used universally!", "/help general"),
-                Utilities.messageMakerString(", "),
-                Utilities.hoverMaker("Creative", "Commands to use in the Creative world!", "/help creative"),
-                Utilities.messageMakerString(", "),
-                Utilities.hoverMaker("Survival", "Commands to use in the Survival world!", "/help survival"),
-                Utilities.messageMakerString(", "),
-                Utilities.hoverMaker("Clans", "Commands to use in the Clans world!", "/help clans"),
-                Utilities.messageMakerString(", "),
-                Utilities.hoverMaker("SkyBlock", "Commands to use in the SkyBlock world!", "/help skyblock"));
-    }
+		sender.sendMessage(variable2 + "--== [Help] ==--");
+		sender.sendMessage(" ");
+		sender.sendMessage(variable1 + "Usage: " + ChatColor.RED + "/help <option> [page]");
+		sender.sendMessage(variable1 + "Available Options:");
+		sender.spigot().sendMessage(
+				Utilities.hoverMaker("General", "Helpful commands to be used universally!", "/help general"),
+				Utilities.messageMakerString(", "),
+				Utilities.hoverMaker("Creative", "Commands to use in the Creative world!", "/help creative"),
+				Utilities.messageMakerString(", "),
+				Utilities.hoverMaker("Survival", "Commands to use in the Survival world!", "/help survival"),
+				Utilities.messageMakerString(", "),
+				Utilities.hoverMaker("Clans", "Commands to use in the Clans world!", "/help clans"),
+				Utilities.messageMakerString(", "),
+				Utilities.hoverMaker("SkyBlock", "Commands to use in the SkyBlock world!", "/help skyblock"));
+	}
 
-    @SubCommand("generalHelp")
-    public void generalHelp(CommandSender sender, ArgumentProvider args) {
-        sender.sendMessage(variable2 + "--== [Help] ==--");
-        sender.sendMessage(" ");
-        sender.sendMessage(variable1 + "General Commands:");
-        sender.sendMessage(string + "/token - Get Your VaultMC Services token.");
-        sender.sendMessage(string + "/discord - Join our Discord.");
-        sender.sendMessage(string + "/msg <sender> - Send a message to a sender.");
-        sender.sendMessage(string + "/tpa - Send a teleport request.");
-        sender.sendMessage(string + "/tpaccept - Accept a teleport request.");
-        sender.sendMessage(string + "/spawn - Return to spawn.");
-        sender.sendMessage(string + "/playtime [player] - Check your PlayTime.");
-    }
+	@SubCommand("generalHelp")
+	public void generalHelp(CommandSender sender, ArgumentProvider args) {
+		sender.sendMessage(variable2 + "--== [Help] ==--");
+		sender.sendMessage(" ");
+		sender.sendMessage(variable1 + "General Commands:");
+		sender.sendMessage(string + "/token - Get Your VaultMC Services token.");
+		sender.sendMessage(string + "/discord - Join our Discord.");
+		sender.sendMessage(string + "/msg <sender> - Send a message to a sender.");
+		sender.sendMessage(string + "/tpa - Send a teleport request.");
+		sender.sendMessage(string + "/tpaccept - Accept a teleport request.");
+		sender.sendMessage(string + "/spawn - Return to spawn.");
+		sender.sendMessage(string + "/playtime [player] - Check your PlayTime.");
+	}
 
-    @SubCommand("creativeHelp")
-    public void creativeHelp(CommandSender sender, ArgumentProvider args) {
-        sender.sendMessage(variable2 + "--== [Help] ==--");
-        sender.sendMessage(" ");
-        sender.sendMessage(variable1 + "Creative Commands:");
-        sender.sendMessage(string + "/plot auto - Claim a plot.");
-        sender.sendMessage(string + "/plot home [player] - Go to a plot.");
-        sender.sendMessage(string + "/plot add <player> - Let a player build on your plot.");
-        sender.sendMessage(string + "/plot remove <player> - Do not a player build on your plot.");
-    }
+	@SubCommand("creativeHelp")
+	public void creativeHelp(CommandSender sender, ArgumentProvider args) {
+		sender.sendMessage(variable2 + "--== [Help] ==--");
+		sender.sendMessage(" ");
+		sender.sendMessage(variable1 + "Creative Commands:");
+		sender.sendMessage(string + "/plot auto - Claim a plot.");
+		sender.sendMessage(string + "/plot home [player] - Go to a plot.");
+		sender.sendMessage(string + "/plot add <player> - Let a player build on your plot.");
+		sender.sendMessage(string + "/plot remove <player> - Do not a player build on your plot.");
+	}
 
-    @SubCommand("survivalHelp")
-    public void survivalHelp(CommandSender sender, ArgumentProvider args) {
-        sender.sendMessage(variable2 + "--== [Help] ==--");
-        sender.sendMessage(" ");
-        sender.sendMessage(variable1 + "Survival Commands:");
-        sender.sendMessage(string + "/wild - Teleport randomly in the world.");
-    }
+	@SubCommand("survivalHelp")
+	public void survivalHelp(CommandSender sender, ArgumentProvider args) {
+		sender.sendMessage(variable2 + "--== [Help] ==--");
+		sender.sendMessage(" ");
+		sender.sendMessage(variable1 + "Survival Commands:");
+		sender.sendMessage(string + "/wild - Teleport randomly in the world.");
+	}
 
-    @SubCommand("clansHelp")
-    public void clansHelp(CommandSender sender, ArgumentProvider args) {
-        sender.sendMessage(variable2 + "--== [Help] ==--");
-        sender.sendMessage(" ");
-        sender.sendMessage(variable1 + "Clans Commands:");
-        sender.sendMessage(string + "/wild - Teleport randomly in the world.");
-        sender.sendMessage(string + "/c join <clan> - Join a clan.");
-        sender.sendMessage(string + "/c home - Go to your clan home.");
-        sender.sendMessage(string + "/c auction - Open the clan auction GUI.");
-    }
+	@SubCommand("clansHelp")
+	public void clansHelp(CommandSender sender, ArgumentProvider args) {
+		sender.sendMessage(variable2 + "--== [Help] ==--");
+		sender.sendMessage(" ");
+		sender.sendMessage(variable1 + "Clans Commands:");
+		sender.sendMessage(string + "/wild - Teleport randomly in the world.");
+		sender.sendMessage(string + "/c join <clan> - Join a clan.");
+		sender.sendMessage(string + "/c home - Go to your clan home.");
+		sender.sendMessage(string + "/c auction - Open the clan auction GUI.");
+	}
 
-    @SubCommand("skyblockHelp")
-    public void skyblockHelp(CommandSender sender, ArgumentProvider args) {
-        sender.sendMessage(variable2 + "--== [Help] ==--");
-        sender.sendMessage(" ");
-        sender.sendMessage(variable1 + "SkyBlock Commands:");
-        sender.sendMessage(string + "/island - Get your own island.");
-        sender.sendMessage(string + "/is - Go to your island");
-        sender.sendMessage(string + "/island top - View the top islands.");
-    }
+	@SubCommand("skyblockHelp")
+	public void skyblockHelp(CommandSender sender, ArgumentProvider args) {
+		sender.sendMessage(variable2 + "--== [Help] ==--");
+		sender.sendMessage(" ");
+		sender.sendMessage(variable1 + "SkyBlock Commands:");
+		sender.sendMessage(string + "/island - Get your own island.");
+		sender.sendMessage(string + "/is - Go to your island");
+		sender.sendMessage(string + "/island top - View the top islands.");
+	}
 }
--- src/net/vaultmc/vaultcore/commands/MsgCommand.java
+++ src/net/vaultmc/vaultcore/commands/MsgCommand.java
@@ -1,121 +1,99 @@
 package net.vaultmc.vaultcore.commands;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.UUID;
+import net.vaultmc.vaultutils.utils.commands.experimental.*;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import com.mojang.brigadier.arguments.StringArgumentType;
 
 import net.vaultmc.vaultcore.Permissions;
 import net.vaultmc.vaultcore.Utilities;
 import net.vaultmc.vaultcore.VaultCore;
 
+@RootCommand(literal = "msg", description = "Send a player a message.")
+@Permission(Permissions.MsgCommand)
+@PlayerOnly
+public class MsgCommand extends CommandExecutor {
 
-public class MsgCommand implements CommandExecutor {
+	private static HashMap<UUID, UUID> replies = new HashMap<>();
 
-    private static HashMap<UUID, UUID> replies = new HashMap<>();
+	public MsgCommand() {
+		this.register("msg", Arrays.asList(
+				Commands.literal("msg"), 
+				Commands.argument("target", EntityArgument.player(),
+				Commands.argument("message", StringArgumentType.greedyString())), 
+				"VaultCore"));
+		
+		this.register("r", Arrays.asList(
+				Commands.literal("r"), 
+				Commands.argument("message", StringArgumentType.greedyString()), 
+				"VaultCore"));
+	}
 
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+	@SubCommand("msg")
+	public void msg(CommandSender sender, ArgumentProvider args) {
 
-        if (commandLabel.equalsIgnoreCase("msg")) {
+		Player player = (Player) sender;
 
-            if (!(sender instanceof Player)) {
-                sender.sendMessage(Utilities.consoleError());
-                return true;
-            }
-            Player player = (Player) sender;
-            if (!player.hasPermission(Permissions.MsgCommand)) {
-                player.sendMessage(Utilities.noPermission());
-                return true;
-            } else if (args.length <= 1) {
-                player.sendMessage(Utilities.usageMessage(cmd.getName(), "<player> <message>"));
-                return true;
-            } else if (args.length >= 2) {
-                Player target = Bukkit.getServer().getPlayer(args[0]);
+		Player target = Bukkit.getPlayer(args.getString("target"));
 
-                if (target == null) {
-                    player.sendMessage(ChatColor.RED + "That player is offline!");
-                    return true;
-                }
-                if (target == player) {
-                    player.sendMessage(ChatColor.RED + "You can't message yourself!");
-                    return true;
-                }
-                if (!VaultCore.getInstance().getPlayerData()
-                        .getBoolean("players." + target.getUniqueId() + ".settings.msg")) {
-                    player.sendMessage(ChatColor.RED + "That player has disabled Messages!");
-                    return true;
-                } else {
+		if (target == null) {
+			player.sendMessage(ChatColor.RED + "That player is offline!");
+		}
+		if (target == player) {
+			player.sendMessage(ChatColor.RED + "You can't message yourself!");
+		}
+		if (!VaultCore.getInstance().getPlayerData().getBoolean("players." + target.getUniqueId() + ".settings.msg")) {
+			player.sendMessage(ChatColor.RED + "That player has disabled Messages!");
+		} else {
 
-                    String message = "";
-                    for (int i = 1; i != args.length; i++)
-                        message += args[i] + " ";
+			String message = args.getString("message");
 
-                    String meTo = (ChatColor.GOLD + player.getName() + ChatColor.YELLOW + " -> " + ChatColor.GOLD
-                            + target.getName() + ChatColor.YELLOW + ":");
-                    String toMe = (ChatColor.GOLD + player.getName() + ChatColor.YELLOW + " -> " + ChatColor.GOLD
-                            + target.getName() + ChatColor.YELLOW + ":");
+			String meTo = (ChatColor.GOLD + player.getName() + ChatColor.YELLOW + " -> " + ChatColor.GOLD
+					+ target.getName() + ChatColor.YELLOW + ":");
+			String toMe = (ChatColor.GOLD + player.getName() + ChatColor.YELLOW + " -> " + ChatColor.GOLD
+					+ target.getName() + ChatColor.YELLOW + ":");
 
-                    player.sendMessage(meTo + " " + ChatColor.DARK_GREEN + message);
-                    target.sendMessage(toMe + " " + ChatColor.DARK_GREEN + message);
+			player.sendMessage(meTo + " " + ChatColor.DARK_GREEN + message);
+			target.sendMessage(toMe + " " + ChatColor.DARK_GREEN + message);
 
-                    replies.put(target.getUniqueId(), player.getUniqueId());
+			replies.put(target.getUniqueId(), player.getUniqueId());
+		}
+	}
 
-                    return true;
-                }
-            }
-        }
+	@SubCommand("r")
+	public void r(CommandSender sender, ArgumentProvider args) {
 
-        if (commandLabel.equalsIgnoreCase("r")) {
+		Player player = (Player) sender;
 
-            if (!(sender instanceof Player)) {
-                sender.sendMessage(Utilities.consoleError());
-                return true;
-            }
+		Player target = Bukkit.getPlayer(replies.get(player.getUniqueId()));
 
-            Player player = (Player) sender;
+		if (target == null) {
+			player.sendMessage(ChatColor.RED + "That player is now offline!");
+		}
+		if (!VaultCore.getInstance().getPlayerData().getBoolean("players." + target.getUniqueId() + ".settings.msg")) {
+			player.sendMessage(ChatColor.RED + "That player has disabled Messages!");
+		}
+		if (replies.containsKey(player.getUniqueId())) {
+			String message = args.getString("message");
 
-            if (!player.hasPermission(Permissions.MsgCommand)) {
-                player.sendMessage(Utilities.noPermission());
-                return true;
-            }
-            if (args.length < 1) {
-                player.sendMessage(Utilities.usageMessage(commandLabel, "<message>"));
-                return true;
-            }
-
-            Player target = Bukkit.getPlayer(replies.get(player.getUniqueId()));
-
-            if (target == null) {
-                player.sendMessage(ChatColor.RED + "That player is now offline!");
-                return true;
-            }
-            if (!VaultCore.getInstance().getPlayerData()
-                    .getBoolean("players." + target.getUniqueId() + ".settings.msg")) {
-                player.sendMessage(ChatColor.RED + "That player has disabled Messages!");
-                return true;
-            }
-            if (replies.containsKey(player.getUniqueId())) {
-                String message = "";
-                for (String s : args) {
-                    message = message + s + " ";
-                }
-                String meTo = (ChatColor.GOLD + player.getName() + ChatColor.YELLOW + " -> " + ChatColor.GOLD
-                        + target.getName() + ChatColor.YELLOW + ":");
-                String toMe = (ChatColor.GOLD + player.getName() + ChatColor.YELLOW + " -> " + ChatColor.GOLD
-                        + target.getName() + ChatColor.YELLOW + ":");
-                player.sendMessage(meTo + " " + ChatColor.DARK_GREEN + message);
-                target.sendMessage(toMe + " " + ChatColor.DARK_GREEN + message);
-                replies.put(target.getUniqueId(), player.getUniqueId());
-            } else {
-                player.sendMessage(ChatColor.RED + "You do not have anyone to reply to!");
-                return true;
-            }
-        }
-        return true;
-    }
+			String meTo = (ChatColor.GOLD + player.getName() + ChatColor.YELLOW + " -> " + ChatColor.GOLD
+					+ target.getName() + ChatColor.YELLOW + ":");
+			String toMe = (ChatColor.GOLD + player.getName() + ChatColor.YELLOW + " -> " + ChatColor.GOLD
+					+ target.getName() + ChatColor.YELLOW + ":");
+			player.sendMessage(meTo + " " + ChatColor.DARK_GREEN + message);
+			target.sendMessage(toMe + " " + ChatColor.DARK_GREEN + message);
+			replies.put(target.getUniqueId(), player.getUniqueId());
+		} else {
+			player.sendMessage(ChatColor.RED + "You do not have anyone to reply to!");
+		}
+	}
 }
