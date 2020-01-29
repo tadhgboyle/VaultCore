7f692d4cd439e821492316545b571d458f4d8d9c
yangyang200 <lin_dl@126.com>
2020-01-29 15:56:58 +0800

    Try to fix SleepHandler

--- src/net/vaultmc/vaultcore/Registry.java
+++ src/net/vaultmc/vaultcore/Registry.java
@@ -1,19 +1,6 @@
 package net.vaultmc.vaultcore;
 
-import net.vaultmc.vaultcore.commands.AFKCommand;
-import net.vaultmc.vaultcore.commands.BackCommand;
-import net.vaultmc.vaultcore.commands.DiscordCommand;
-import net.vaultmc.vaultcore.commands.HelpCommand;
-import net.vaultmc.vaultcore.commands.ListCommand;
-import net.vaultmc.vaultcore.commands.PingCommand;
-import net.vaultmc.vaultcore.commands.PlayTime;
-import net.vaultmc.vaultcore.commands.RanksCommand;
-import net.vaultmc.vaultcore.commands.ReloadCommand;
-import net.vaultmc.vaultcore.commands.SeenCommand;
-import net.vaultmc.vaultcore.commands.StatsCommand;
-import net.vaultmc.vaultcore.commands.TokenCommand;
-import net.vaultmc.vaultcore.commands.WarpCommand;
-import net.vaultmc.vaultcore.commands.WildTeleport;
+import net.vaultmc.vaultcore.commands.*;
 import net.vaultmc.vaultcore.commands.economy.EconomyCommand;
 import net.vaultmc.vaultcore.commands.economy.MoneyCommand;
 import net.vaultmc.vaultcore.commands.economy.TransferCommand;
@@ -24,58 +11,22 @@ import net.vaultmc.vaultcore.commands.report.ReportCommand;
 import net.vaultmc.vaultcore.commands.report.ReportsCommand;
 import net.vaultmc.vaultcore.commands.settings.SettingsCommand;
 import net.vaultmc.vaultcore.commands.settings.SettingsListener;
-import net.vaultmc.vaultcore.commands.staff.BrandCommand;
-import net.vaultmc.vaultcore.commands.staff.CheckCommand;
-import net.vaultmc.vaultcore.commands.staff.ClearChatCommand;
-import net.vaultmc.vaultcore.commands.staff.ConsoleSay;
-import net.vaultmc.vaultcore.commands.staff.FeedCommand;
-import net.vaultmc.vaultcore.commands.staff.FlyCommand;
-import net.vaultmc.vaultcore.commands.staff.HasPermCommand;
-import net.vaultmc.vaultcore.commands.staff.HealCommand;
-import net.vaultmc.vaultcore.commands.staff.InvseeCommand;
-import net.vaultmc.vaultcore.commands.staff.ModMode;
-import net.vaultmc.vaultcore.commands.staff.MuteChatCommand;
-import net.vaultmc.vaultcore.commands.staff.StaffChatCommand;
-import net.vaultmc.vaultcore.commands.staff.TagCommand;
-import net.vaultmc.vaultcore.commands.staff.TimeCommand;
-import net.vaultmc.vaultcore.commands.staff.VanishCommand;
-import net.vaultmc.vaultcore.commands.staff.WeatherCommand;
+import net.vaultmc.vaultcore.commands.staff.*;
 import net.vaultmc.vaultcore.commands.staff.gamemode.GMCreativeCommand;
 import net.vaultmc.vaultcore.commands.staff.gamemode.GMSpectatorCommand;
 import net.vaultmc.vaultcore.commands.staff.gamemode.GMSurvivalCommand;
 import net.vaultmc.vaultcore.commands.staff.gamemode.GameModeCommand;
 import net.vaultmc.vaultcore.commands.staff.grant.GrantCommand;
 import net.vaultmc.vaultcore.commands.staff.grant.GrantCommandListener;
-import net.vaultmc.vaultcore.commands.staff.punishments.ban.BanCommand;
-import net.vaultmc.vaultcore.commands.staff.punishments.ban.BannedListener;
-import net.vaultmc.vaultcore.commands.staff.punishments.ban.IpBanCommand;
-import net.vaultmc.vaultcore.commands.staff.punishments.ban.IpTempBanCommand;
-import net.vaultmc.vaultcore.commands.staff.punishments.ban.TempBanCommand;
-import net.vaultmc.vaultcore.commands.staff.punishments.ban.UnbanCommand;
+import net.vaultmc.vaultcore.commands.staff.punishments.ban.*;
 import net.vaultmc.vaultcore.commands.staff.punishments.kick.KickCommand;
-import net.vaultmc.vaultcore.commands.staff.punishments.mute.IpMuteCommand;
-import net.vaultmc.vaultcore.commands.staff.punishments.mute.IpTempMuteCommand;
-import net.vaultmc.vaultcore.commands.staff.punishments.mute.MuteCommand;
-import net.vaultmc.vaultcore.commands.staff.punishments.mute.MutedListener;
-import net.vaultmc.vaultcore.commands.staff.punishments.mute.TempMuteCommand;
-import net.vaultmc.vaultcore.commands.staff.punishments.mute.UnmuteCommand;
-import net.vaultmc.vaultcore.commands.teleport.TPACommand;
-import net.vaultmc.vaultcore.commands.teleport.TPAHereCommand;
-import net.vaultmc.vaultcore.commands.teleport.TPAcceptCommand;
-import net.vaultmc.vaultcore.commands.teleport.TPCommand;
-import net.vaultmc.vaultcore.commands.teleport.TPDenyCommand;
-import net.vaultmc.vaultcore.commands.teleport.TPHereCommand;
+import net.vaultmc.vaultcore.commands.staff.punishments.mute.*;
+import net.vaultmc.vaultcore.commands.teleport.*;
 import net.vaultmc.vaultcore.commands.worldtp.CRCommand;
 import net.vaultmc.vaultcore.commands.worldtp.SVCommand;
-import net.vaultmc.vaultcore.listeners.ChatUtils;
-import net.vaultmc.vaultcore.listeners.CycleListener;
-import net.vaultmc.vaultcore.listeners.GameModeListeners;
-import net.vaultmc.vaultcore.listeners.PlayerJoinQuitListener;
-import net.vaultmc.vaultcore.listeners.PlayerTPListener;
-import net.vaultmc.vaultcore.listeners.ShutDownListener;
-import net.vaultmc.vaultcore.listeners.SignColours;
-import net.vaultmc.vaultcore.listeners.VanishListeners;
+import net.vaultmc.vaultcore.listeners.*;
 import net.vaultmc.vaultcore.ported.inventory.InventoryStorageListeners;
+import net.vaultmc.vaultcore.survival.SleepHandler;
 
 public class Registry {
 	private static final VaultCore vault = VaultCore.getInstance();
@@ -160,8 +111,6 @@ public class Registry {
 		vault.registerEvents(new ShutDownListener());
         vault.registerEvents(new BannedListener());
         vault.registerEvents(new MutedListener());
-        /*
         vault.registerEvents(new SleepHandler());
-        */
 	}
 }
--- src/net/vaultmc/vaultcore/survival/SleepHandler.java
+++ src/net/vaultmc/vaultcore/survival/SleepHandler.java
@@ -1,19 +1,54 @@
-/*
 package net.vaultmc.vaultcore.survival;
 
+import net.vaultmc.vaultloader.VaultLoader;
 import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
-
-import net.vaultmc.vaultcore.Utilities;
-import net.vaultmc.vaultloader.VaultLoader;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
 
 public class SleepHandler implements Listener {
+    private static final World world = Bukkit.getWorld("Survival");
+    private static int sleepingPlayers = 0;
 
+    private static void showActionbar() {
+        if (sleepingPlayers < Math.round(world.getPlayerCount() / 2D)) {
+            for (Player player : world.getPlayers()) {
+                player.sendActionBar(VaultLoader.getMessage("vaultcore.survival.players_needed").replace("{PLAYERS}",
+                        String.valueOf(Math.round(world.getPlayerCount() / 2D) - sleepingPlayers)));
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerWantsToRestAndHaveADream(PlayerBedEnterEvent e) {
+        if (e.getPlayer().getWorld() == world && e.getBedEnterResult() == PlayerBedEnterEvent.BedEnterResult.OK) {
+            sleepingPlayers++;
+            showActionbar();
+
+            if (sleepingPlayers >= Math.round(world.getPlayerCount() / 2D)) {
+                world.setTime(0);
+                world.setStorm(false);
+                world.setThundering(false);
+                sleepingPlayers = 0;
+                for (Player player : world.getPlayers()) {
+                    player.sendMessage(VaultLoader.getMessage("vaultcore.survival.time_set_day"));
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerWakesUpEarlyToPlayMinecraft(PlayerBedLeaveEvent e) {
+        if (e.getPlayer().getWorld().getName().equals("Survival")) {
+            if (sleepingPlayers > 0) sleepingPlayers--;
+            showActionbar();
+        }
+    }
+
+    /*
 	int sleeping = 0;
 	int survival = 0;
 
@@ -58,5 +93,5 @@ public class SleepHandler implements Listener {
 			}
 		}
 	}
+     */
 }
-*/
