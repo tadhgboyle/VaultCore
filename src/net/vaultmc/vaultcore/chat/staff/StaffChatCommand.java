d580bdb52af12ffcc91d6fae09a4c846048f0a1c
yangyang200 <lin_dl@126.com>
2020-02-12 09:08:18 +0800

    Revert faulty change

--- src/net/vaultmc/vaultcore/chat/staff/StaffChatCommand.java
+++ src/net/vaultmc/vaultcore/chat/staff/StaffChatCommand.java
@@ -1,16 +1,12 @@
 package net.vaultmc.vaultcore.chat.staff;
 
-import lombok.SneakyThrows;
 import net.vaultmc.vaultcore.Permissions;
 import net.vaultmc.vaultcore.Utilities;
 import net.vaultmc.vaultloader.VaultLoader;
 import net.vaultmc.vaultloader.utils.commands.*;
 import net.vaultmc.vaultloader.utils.player.VLCommandSender;
 import net.vaultmc.vaultloader.utils.player.VLPlayer;
-import org.bukkit.Bukkit;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -28,53 +24,27 @@ public class StaffChatCommand extends CommandExecutor {
     }
 
     @SubCommand("chat")
-    @SneakyThrows
-    public static void chat(VLCommandSender sender, String message) {
-        ByteArrayOutputStream extern = new ByteArrayOutputStream();
-        DataOutputStream dos = new DataOutputStream(extern);
-        dos.writeUTF("Forward");
-        dos.writeUTF("ALL");
-
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        DataOutputStream stream = new DataOutputStream(os);
-        stream.writeUTF("Send");
-        stream.writeUTF(sender.getFormattedName());
-        stream.writeUTF(message);
-        byte[] bytes = os.toByteArray();
-        dos.writeShort(bytes.length);
-        dos.write(bytes);
-        stream.close();
-
-        Bukkit.getWorlds().get(0).sendPluginMessage(VaultLoader.getInstance(), "BungeeCord", extern.toByteArray());
-        dos.close();
+    public void chat(VLCommandSender sender, String message) {
+        for (VLPlayer player : VLPlayer.getOnlinePlayers()) {
+            if (player.hasPermission(Permissions.StaffChatCommand)) {
+                player.sendMessage(VaultLoader.getMessage("vaultcore.commands.staffchat.prefix")
+                        + Utilities.formatMessage(VaultLoader.getMessage("vaultcore.commands.staffchat.format"),
+                        sender.getFormattedName(), message));
+            }
+        }
     }
 
     @SubCommand("toggle")
     @PlayerOnly
-    @SneakyThrows
     public void toggle(VLPlayer player) {
-        ByteArrayOutputStream extern = new ByteArrayOutputStream();
-        DataOutputStream dos = new DataOutputStream(extern);
-        dos.writeUTF("Forward");
-        dos.writeUTF("ALL");
-
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        DataOutputStream stream = new DataOutputStream(os);
-        stream.writeUTF("SetAlwaysOn");
-        stream.writeUTF(player.getUniqueId().toString());
         if (toggled.contains(player.getUniqueId())) {
-            stream.writeBoolean(false);
+            toggled.remove(player.getUniqueId());
             player.sendMessage(
                     Utilities.formatMessage(VaultLoader.getMessage("vaultcore.commands.staffchat.toggle"), "off"));
         } else {
-            stream.writeBoolean(true);
+            toggled.add(player.getUniqueId());
             player.sendMessage(
                     Utilities.formatMessage(VaultLoader.getMessage("vaultcore.commands.staffchat.toggle"), "on"));
         }
-        byte[] bytes = os.toByteArray();
-        dos.writeShort(bytes.length);
-        dos.write(bytes);
-        stream.close();
-        player.getPlayer().sendPluginMessage(VaultLoader.getInstance(), "BungeeCord", extern.toByteArray());
     }
 }
