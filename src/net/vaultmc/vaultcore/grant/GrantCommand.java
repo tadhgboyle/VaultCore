70574d1745a4c1ec50d4ffac0e2bce2315bf7ca3
Aberdeener <tadhgsmboyle@gmail.com>
2020-02-12 18:45:20 -0800

    make staff see vanished, only let mod promote non staff

--- src/net/vaultmc/vaultcore/grant/GrantCommand.java
+++ src/net/vaultmc/vaultcore/grant/GrantCommand.java
@@ -1,6 +1,7 @@
 package net.vaultmc.vaultcore.grant;
 
 import net.vaultmc.vaultcore.Permissions;
+import net.vaultmc.vaultloader.VaultLoader;
 import net.vaultmc.vaultloader.utils.commands.*;
 import net.vaultmc.vaultloader.utils.player.VLPlayer;
 import org.bukkit.ChatColor;
@@ -27,6 +28,9 @@ public class GrantCommand extends CommandExecutor {
         if (player.hasPermission(Permissions.GrantCommandAdmin)) {
             player.openInventory(GrantCommandInv.getGrantInventoryAdmin(target));
         } else if (player.hasPermission(Permissions.GrantCommandMod)) {
+        	if (target.getGroup().equalsIgnoreCase("admin") || target.getGroup().equalsIgnoreCase("moderator")) {
+        		player.sendMessage(VaultLoader.getMessage("vaultcore.commands.grant.mod-no-perm"));
+        	}
             player.openInventory(GrantCommandInv.getGrantInventoryMod(target));
         }
     }
