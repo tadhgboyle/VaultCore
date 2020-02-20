0f767d72c5f726a297524b5b854aa5ab94a1363f
yangyang200 <lin_dl@126.com>
2020-02-20 14:36:39 +0800

    Fix server navigator NPE

--- src/net/vaultmc/vaultcore/misc/listeners/ServerNavigator.java
+++ src/net/vaultmc/vaultcore/misc/listeners/ServerNavigator.java
@@ -159,7 +159,8 @@ public class ServerNavigator extends ConstructorRegisterListener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent e) {
-        if (e.getAction() == Action.RIGHT_CLICK_AIR && e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals(ChatColor.GREEN + "Server Navigator")) {
+        if (e.getAction() == Action.RIGHT_CLICK_AIR && e.getPlayer().getInventory().getItemInMainHand() != null &&
+                e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals(ChatColor.GREEN + "Server Navigator")) {
             e.getPlayer().openInventory(inv);
         }
     }
