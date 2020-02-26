40f5c91e861089ed1258818338f1f35a1b21ef97
Aberdeener <tadhgsmboyle@gmail.com>
2020-02-25 16:05:50 -0800

    fix crash message, add bot status command

--- src/net/vaultmc/vaultcore/discordbot/ManageBotCommand.java
+++ src/net/vaultmc/vaultcore/discordbot/ManageBotCommand.java
@@ -5,6 +5,7 @@ import net.vaultmc.vaultcore.Utilities;
 import net.vaultmc.vaultloader.VaultLoader;
 import net.vaultmc.vaultloader.utils.commands.*;
 import net.vaultmc.vaultloader.utils.player.VLCommandSender;
+import org.bukkit.ChatColor;
 
 import java.util.Collections;
 
@@ -12,12 +13,18 @@ import java.util.Collections;
 @Permission(Permissions.VaultMCBotManage)
 public class ManageBotCommand extends CommandExecutor {
     public ManageBotCommand() {
+        register("status", Collections.emptyList());
         register("stop",
                 Collections.singletonList(Arguments.createLiteral("stop")));
         register("start",
                 Collections.singletonList(Arguments.createLiteral("start")));
         register("restart",
                 Collections.singletonList(Arguments.createLiteral("restart")));
+    }
+
+    @SubCommand("status")
+    public static void status(VLCommandSender sender) {
+        sender.sendMessage(Utilities.formatMessage(VaultLoader.getMessage("vaultcore.commands.managebot.status"), VaultMCBot.isStarted() ? ChatColor.GREEN + "up" : ChatColor.RED + "down"));
     }
 
     @SubCommand("stop")
--- src/net/vaultmc/vaultcore/misc/commands/staff/CrashCommand.java
+++ src/net/vaultmc/vaultcore/misc/commands/staff/CrashCommand.java
@@ -1,6 +1,7 @@
 package net.vaultmc.vaultcore.misc.commands.staff;
 
 import net.vaultmc.vaultcore.Permissions;
+import net.vaultmc.vaultcore.Utilities;
 import net.vaultmc.vaultloader.VaultLoader;
 import net.vaultmc.vaultloader.utils.commands.*;
 import net.vaultmc.vaultloader.utils.player.VLCommandSender;
@@ -22,6 +23,6 @@ public class CrashCommand extends CommandExecutor {
     @SubCommand("crash")
     public void crash(VLCommandSender sender, VLPlayer target) {
         target.getPlayer().spawnParticle(Particle.SPELL_WITCH, target.getLocation(), 1000000000);
-        sender.sendMessage(VaultLoader.getMessage("useful"));
+        sender.sendMessage(Utilities.formatMessage(VaultLoader.getMessage("useful"), target.getFormattedName()));
     }
 }
