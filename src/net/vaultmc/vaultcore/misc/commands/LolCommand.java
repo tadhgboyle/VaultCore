5af58d4a8e373c28bd20420cde927b586e3218df
yangyang200 <lin_dl@126.com>
2020-02-27 18:23:20 +0800

    Update lol to use Gson instead

--- src/net/vaultmc/vaultcore/misc/commands/LolCommand.java
+++ src/net/vaultmc/vaultcore/misc/commands/LolCommand.java
@@ -1,5 +1,9 @@
 package net.vaultmc.vaultcore.misc.commands;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 import net.vaultmc.vaultcore.Permissions;
 import net.vaultmc.vaultcore.Utilities;
 import net.vaultmc.vaultcore.VaultCore;
@@ -10,9 +14,6 @@ import net.vaultmc.vaultloader.utils.player.VLPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 import java.io.File;
 import java.io.FileReader;
@@ -29,20 +30,20 @@ import java.util.concurrent.ThreadLocalRandom;
 public class LolCommand extends CommandExecutor {
 
     public static File lolFile = new File(VaultCore.getInstance().getDataFolder() + "/lols.json");
-    private static JSONArray lolsList;
+    private static JsonArray lolsList;
     private static FileWriter updatedLols;
     private static long LAST_LOL = 0;
     private static long COOLDOWN = 15000;
     private static int PAGE_SIZE = 7;
 
-    JSONParser parser = new JSONParser();
+    JsonParser parser = new JsonParser();
     Object obj;
 
     {
         try {
             obj = parser.parse(new FileReader(lolFile));
-            JSONObject jsonObject = (JSONObject) obj;
-            lolsList = (JSONArray) jsonObject.get("lols");
+            JsonObject object = (JsonObject) obj;
+            lolsList = object.getAsJsonArray("lols");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -70,7 +71,7 @@ public class LolCommand extends CommandExecutor {
             }
             LAST_LOL = System.currentTimeMillis();
         } else {
-            int secs = (14 - (int) Math.ceil((System.currentTimeMillis() - LAST_LOL) / 1000));
+            int secs = (14 - (int) Math.ceil((System.currentTimeMillis() - LAST_LOL) / 1000D));
             sender.sendMessage(Utilities.formatMessage(VaultLoader.getMessage("vaultcore.commands.lol.cooldown"), secs));
         }
     }
@@ -90,7 +91,7 @@ public class LolCommand extends CommandExecutor {
                 sender.sendMessage(Utilities.formatMessage(VaultLoader.getMessage("vaultcore.commands.lol.max"), id, lolsList.size() - 1));
             }
         } else {
-            int secs = (14 - (int) Math.ceil((System.currentTimeMillis() - LAST_LOL) / 1000));
+            int secs = (14 - (int) Math.ceil((System.currentTimeMillis() - LAST_LOL) / 1000D));
             sender.sendMessage(Utilities.formatMessage(VaultLoader.getMessage("vaultcore.commands.lol.cooldown"), secs));
         }
     }
@@ -134,7 +135,7 @@ public class LolCommand extends CommandExecutor {
     @SubCommand("lolAdd")
     @Permission(Permissions.LolCommandEdit)
     public static void lolAdd(VLCommandSender sender, String text) {
-        if (!lolsList.contains(text)) {
+        if (!lolsList.contains(new JsonPrimitive(text))) {
             lolsList.add(text);
             if (saveLols()) {
                 sender.sendMessage(Utilities.formatMessage(VaultLoader.getMessage("vaultcore.commands.lol.add.added"), lolsList.size() - 1));
@@ -142,7 +143,7 @@ public class LolCommand extends CommandExecutor {
                 sender.sendMessage(Utilities.formatMessage(VaultLoader.getMessage("vaultcore.commands.lol.add.not_added"), lolsList.size() - 1));
             }
         } else {
-            sender.sendMessage(Utilities.formatMessage(VaultLoader.getMessage("vaultcore.commands.lol.add.already_exists"), lolsList.indexOf(text)));
+            sender.sendMessage(Utilities.formatMessage(VaultLoader.getMessage("vaultcore.commands.lol.add.already_exists")));
         }
     }
 
@@ -165,22 +166,16 @@ public class LolCommand extends CommandExecutor {
 
     public static boolean saveLols() {
         try {
-            JSONObject lolsEdited = new JSONObject();
-            lolsEdited.put("lols", lolsList);
+            JsonObject lolsEdited = new JsonObject();
+            lolsEdited.add("lols", lolsList);
             updatedLols = new FileWriter(lolFile);
-            updatedLols.write(String.valueOf(lolsEdited));
+            updatedLols.write(lolsEdited.toString());
+            updatedLols.flush();
+            updatedLols.close();
         } catch (IOException e) {
             e.printStackTrace();
             return false;
-        } finally {
-            try {
-                updatedLols.flush();
-                updatedLols.close();
-                return true;
-            } catch (IOException e) {
-                e.printStackTrace();
-                return false;
-            }
         }
+        return true;
     }
 }
