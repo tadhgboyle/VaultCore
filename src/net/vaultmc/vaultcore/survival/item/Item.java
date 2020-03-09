e7a862c1681528351afe165929fd9de6f15ab10b
yangyang200 <lin_dl@126.com>
2020-03-09 11:07:12 +0800

    Basic Item

---
+++ src/net/vaultmc/vaultcore/survival/item/Item.java
@@ -0,0 +1,45 @@
+package net.vaultmc.vaultcore.survival.item;
+
+import lombok.Getter;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.StringTag;
+import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Item {
+    private static final Map<String, Item> items = new HashMap<>();
+    @Getter
+    private ItemStack item;
+    @Getter
+    private String id;
+
+    public Item(ItemStack item, String id) {
+        net.minecraft.world.item.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+        CompoundTag tag = nmsItem.getOrCreateTag();
+        tag.set("Survival.ItemKey", StringTag.valueOf(id));
+        nmsItem.setTag(tag);
+        this.item = CraftItemStack.asBukkitCopy(nmsItem);
+        this.id = id;
+        items.put(id, this);
+    }
+
+    public static Item getBy(ItemStack item) {
+        net.minecraft.world.item.ItemStack nmsItem = CraftItemStack.asNMSCopy(item);
+        CompoundTag tag = nmsItem.getOrCreateTag();
+        if (tag.contains("Survival.ItemKey")) {
+            return getBy(tag.getString("Survival.ItemKey"));
+        }
+        return null;
+    }
+
+    public static Item getBy(String id) {
+        return items.get(id);
+    }
+
+    public static String getId(ItemStack item) {
+        return getBy(item) != null ? getBy(item).getId() : null;
+    }
+}
