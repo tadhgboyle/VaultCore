16402ad9c33f91a92ef95ca11d4de5e3791bf2e0
yangyang200 <lin_dl@126.com>
2020-03-10 11:07:34 +0800

    Add lapis armor and player head crafting

--- src/net/vaultmc/vaultcore/survival/item/ItemRegistry.java
+++ src/net/vaultmc/vaultcore/survival/item/ItemRegistry.java
@@ -1,5 +1,6 @@
 package net.vaultmc.vaultcore.survival.item;
 
+import com.destroystokyo.paper.event.player.PlayerPickupExperienceEvent;
 import net.vaultmc.vaultcore.survival.item.recipe.ShapedRecipe;
 import net.vaultmc.vaultloader.utils.ConstructorRegisterListener;
 import net.vaultmc.vaultloader.utils.ItemStackBuilder;
@@ -27,6 +28,13 @@ public class ItemRegistry extends ConstructorRegisterListener {
         ItemStack stick = new ItemStack(Material.STICK);
         ItemStack leather = new ItemStack(Material.LEATHER);
         ItemStack dirt = new ItemStack(Material.DIRT);
+        ItemStack itemFrame = new ItemStack(Material.ITEM_FRAME);
+        ItemStack nameTag = new ItemStack(Material.NAME_TAG);
+        new Item(new ItemStack(Material.PLAYER_HEAD), "minecraft:player_head", new ShapedRecipe(new ItemStack[]{
+                itemFrame, null, null,
+                nameTag, null, null,
+                null, null, null
+        }));
         new Item(new ItemStackBuilder(Material.LEATHER_HELMET)
                 .name(ChatColor.RESET + "Soil Helmet")
                 .lore(Arrays.asList(
@@ -159,6 +167,63 @@ public class ItemRegistry extends ConstructorRegisterListener {
                 .build(), "survival:cactus_boots", new ShapedRecipe(new ItemStack[]{
                 cactus, null, cactus,
                 cactus, null, cactus,
+                null, null, null
+        }));
+        ItemStack lapis = new ItemStack(Material.LAPIS_LAZULI);
+        new Item(new ItemStackBuilder(Material.LEATHER_HELMET)
+                .leatherColor(Color.BLUE)
+                .name(ChatColor.RESET + "Lapis Helmet")
+                .lore(Arrays.asList(
+                        ChatColor.GOLD + "" + ChatColor.BOLD + "Full Set Bonus:",
+                        ChatColor.YELLOW + "2x Experience Everywhere"
+                ))
+                .maxDurability(384)
+                .attribute(Attribute.GENERIC_ARMOR, new AttributeModifier(UUID.randomUUID(), "Armor", 3, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HEAD))
+                .build(), "survival:lapis_helmet", new ShapedRecipe(new ItemStack[]{
+                lapis, lapis, lapis,
+                lapis, null, lapis,
+                null, null, null
+        }));
+        new Item(new ItemStackBuilder(Material.LEATHER_CHESTPLATE)
+                .leatherColor(Color.BLUE)
+                .name(ChatColor.RESET + "Lapis Chestplate")
+                .lore(Arrays.asList(
+                        ChatColor.GOLD + "" + ChatColor.BOLD + "Full Set Bonus:",
+                        ChatColor.YELLOW + "2x Experience Everywhere"
+                ))
+                .maxDurability(384)
+                .attribute(Attribute.GENERIC_ARMOR, new AttributeModifier(UUID.randomUUID(), "Armor", 8, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.CHEST))
+                .build(), "survival:lapis_chestplate", new ShapedRecipe(new ItemStack[]{
+                lapis, null, lapis,
+                lapis, lapis, lapis,
+                lapis, lapis, lapis
+        }));
+        new Item(new ItemStackBuilder(Material.LEATHER_LEGGINGS)
+                .leatherColor(Color.BLUE)
+                .name(ChatColor.RESET + "Lapis Leggings")
+                .lore(Arrays.asList(
+                        ChatColor.GOLD + "" + ChatColor.BOLD + "Full Set Bonus:",
+                        ChatColor.YELLOW + "2x Experience Everywhere"
+                ))
+                .maxDurability(384)
+                .attribute(Attribute.GENERIC_ARMOR, new AttributeModifier(UUID.randomUUID(), "Armor", 4, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.LEGS))
+                .build(), "survival:lapis_leggings", new ShapedRecipe(new ItemStack[]{
+                lapis, lapis, lapis,
+                lapis, null, lapis,
+                lapis, null, lapis
+        }));
+        new Item(new ItemStackBuilder(Material.LEATHER_BOOTS)
+                .leatherColor(Color.BLUE)
+                .name(ChatColor.RESET + "Lapis Boots")
+                .lore(Arrays.asList(
+                        ChatColor.GOLD + "" + ChatColor.BOLD + "Full Set Bonus:",
+                        ChatColor.YELLOW + "2x Experience Everywhere"
+                ))
+                .maxDurability(384)
+                .attribute(Attribute.GENERIC_ARMOR, new AttributeModifier(UUID.randomUUID(), "Armor", 2, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.FEET))
+                .build(), "survival:lapis_boots", new ShapedRecipe(new ItemStack[]{
+                lapis, null, lapis,
+                lapis, null, lapis,
                 null, null, null
         }));
 
@@ -356,14 +421,34 @@ public class ItemRegistry extends ConstructorRegisterListener {
         ItemStack netherStar = new ItemStack(Material.NETHER_STAR);
         new Item(new ItemStackBuilder(Material.IRON_SWORD)
                 .name(ChatColor.RESET + "Sword of the Nether Lord")
-                .attribute(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier(UUID.randomUUID(), "ThisIsDesignedFor10YearOldKidsBecauseWhyNotTheyWillLoveTheNamingRight",
+                .attribute(Attribute.GENERIC_ATTACK_DAMAGE, new AttributeModifier(UUID.randomUUID(), "Damage",
                         30, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND))
                 .maxDurability(2000)
-                .build(), "survival:r_slash_minecraft_circlejerk", new ShapedRecipe(new ItemStack[]{
+                .build(), "survival:star_thin_sword", new ShapedRecipe(new ItemStack[]{
                 netherStar, null, null,
                 netherStar, null, null,
                 stick, null, null
         }));
+    }
+
+    public static void load() {
+        // Call static initializer
+        new ItemRegistry();
+    }
+
+    @EventHandler
+    public void onExperienceGet(PlayerPickupExperienceEvent e) {
+        PlayerInventory inv = e.getPlayer().getInventory();
+        int match = 0;
+        for (ItemStack is : inv.getArmorContents()) {
+            if (Item.getId(is) != null && Item.getId(is).matches("survival:lapis_(helmet|chestplate|leggings|boots)")) {
+                match++;
+            }
+        }
+
+        if (match == 4) {
+            e.getExperienceOrb().setExperience(e.getExperienceOrb().getExperience() * 2);
+        }
     }
 
     @EventHandler
@@ -379,11 +464,6 @@ public class ItemRegistry extends ConstructorRegisterListener {
                 ((LivingEntity) e.getDamager()).damage(toReflect);
             }
         }
-    }
-
-    public static void load() {
-        // Call static initializer
-        new ItemRegistry();
     }
 
     @EventHandler
