2c656b0a4387b3629c5b0d9cdf3dc74d5d7ffe68
ZackieChan <2xjtnisthebest@gmail.com>
2020-04-30 20:05:23 -0700

    Simplifying Discord command code

--- src/net/vaultmc/vaultcore/connections/DiscordCommand.java
+++ src/net/vaultmc/vaultcore/connections/DiscordCommand.java
@@ -26,13 +26,7 @@ public class DiscordCommand extends CommandExecutor {
         player.sendMessage(VaultLoader.getMessage("vaultcore.commands.discord.guild"));
         player.sendMessage(Utilities.formatMessage(VaultLoader.getMessage("vaultcore.commands.token.your_token"),
                 token));
-        long id = VLOfflinePlayer.getDiscordUser(player);
 
-        if(id == 0) {
-            player.sendMessage(ChatColor.YELLOW + "Status: " + ChatColor.RED + "Inactive");
-            return;
-        } else {
-            player.sendMessage(ChatColor.YELLOW + "Status: " + ChatColor.GREEN + "Active");
-        }
+        player.sendMessage((VLOfflinePlayer.getDiscordUser(player) == 0)? ChatColor.GREEN + "Enabled" : ChatColor.RED + "Disabled");
     }
 }
