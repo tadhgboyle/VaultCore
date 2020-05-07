592f1588bdaf5fe0407052904befd6abbd16ee3a
Tadhg <tadhgsmboyle@gmail.com>
2020-05-07 10:04:54 -0700

    make HelpCommand look better + permission checks

--- src/net/vaultmc/vaultcore/misc/commands/HelpCommand.java
+++ src/net/vaultmc/vaultcore/misc/commands/HelpCommand.java
@@ -1,15 +1,19 @@
 package net.vaultmc.vaultcore.misc.commands;
 
 import net.vaultmc.vaultcore.Permissions;
+import net.vaultmc.vaultcore.Utilities;
 import net.vaultmc.vaultloader.VaultLoader;
 import net.vaultmc.vaultloader.utils.commands.*;
 import net.vaultmc.vaultloader.utils.player.VLCommandSender;
+import net.vaultmc.vaultloader.utils.player.VLPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.Collections;
 
-@RootCommand(literal = "vchelp", description = "Help me!")
+@RootCommand(literal = "help", description = "Help me!")
 @Permission(Permissions.HelpCommand)
 public class HelpCommand extends CommandExecutor {
 
@@ -27,21 +31,30 @@ public class HelpCommand extends CommandExecutor {
     public void helpCommand(VLCommandSender sender, String command) {
         Class<?> clazz = VaultLoader.getCommandClasses().get(command);
         if (clazz == null) {
-            sender.sendMessage("not vaultloader command");
+            helpMain(sender);
             return;
         }
-        sender.sendMessage("root command: " + clazz.getAnnotation(RootCommand.class).literal());
-        sender.sendMessage("description: " + clazz.getAnnotation(RootCommand.class).description());
-        sender.sendMessage("-------------------------");
+        if (clazz.isAnnotationPresent(Permission.class) && sender instanceof VLPlayer && !((VLPlayer) sender).hasPermission(clazz.getAnnotation(Permission.class).value())) {
+            sender.sendMessage(Bukkit.getPermissionMessage());
+            return;
+        }
+        sender.sendMessage(ChatColor.YELLOW + "Help: " + ChatColor.GOLD + "/" + clazz.getAnnotation(RootCommand.class).literal());
+        sender.sendMessage(ChatColor.YELLOW + "Description: " + ChatColor.GOLD + clazz.getAnnotation(RootCommand.class).description());
+        sender.sendMessage(ChatColor.DARK_GREEN + "-------------------------");
         for (Method method : clazz.getDeclaredMethods()) {
             if (!method.isAnnotationPresent(SubCommand.class)) continue;
-            sender.sendMessage("subcommand: " + method.getAnnotation(SubCommand.class).value());
+            if (method.isAnnotationPresent(Permission.class) && sender instanceof VLPlayer && !((VLPlayer) sender).hasPermission(method.getAnnotation(Permission.class).value()))
+                continue;
+            sender.sendMessage(ChatColor.YELLOW + "Sub-Command: " + ChatColor.GOLD + Utilities.capitalizeMessage(method.getAnnotation(SubCommand.class).value()));
             StringBuilder sb = new StringBuilder();
-            for (Parameter parameter : method.getParameters()) {
-                sb.append(parameter.getType().getClass().getSimpleName()).append(" ");
+            if (method.getParameters().length > 1) {
+                for (Parameter parameter : method.getParameters()) {
+                    if (!parameter.getName().equals("sender"))
+                        sb.append("<").append(parameter.getName()).append("> ");
+                }
             }
-            sender.sendMessage("usage: /" + clazz.getAnnotation(RootCommand.class).literal() + " " + sb.toString());
-            sender.sendMessage("-------------------------");
+            sender.sendMessage(ChatColor.YELLOW + "Usage: " + ChatColor.GOLD + "/" + clazz.getAnnotation(RootCommand.class).literal() + " " + sb.toString().trim());
+            sender.sendMessage(ChatColor.DARK_GREEN + "-------------------------");
         }
     }
 }
