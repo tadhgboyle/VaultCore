3bfee82e96ce92f12676538f494d418baee08254
yangyang200 <lin_dl@126.com>
2020-05-22 10:47:24 +0800

    Add PvP world to InvStorageListeners

--- src/net/vaultmc/vaultcore/inventory/InventoryStorageListeners.java
+++ src/net/vaultmc/vaultcore/inventory/InventoryStorageListeners.java
@@ -43,6 +43,7 @@ public class InventoryStorageListeners extends ConstructorRegisterListener {
         worldGroups.put("clans", new String[]{"clans"});
         worldGroups.put("creative", new String[]{"creative", "legacy_creative"});
         worldGroups.put("rpg", new String[]{"throwback", "throwback2"});
+        worldGroups.put("pvp", new String[]{"pvp"});
     }
 
     public static String getGroupOf(String world) {
