ec071368da615bf2861397c6e783c26d9d564f66
yangyang200 <lin_dl@126.com>
2020-06-06 12:30:29 +0800

    Add auto game mode switch for build world

--- src/net/vaultmc/vaultcore/misc/listeners/GameModeListeners.java
+++ src/net/vaultmc/vaultcore/misc/listeners/GameModeListeners.java
@@ -14,6 +14,7 @@
 package net.vaultmc.vaultcore.misc.listeners;
 
 import net.vaultmc.vaultloader.utils.ConstructorRegisterListener;
+import net.vaultmc.vaultloader.utils.player.VLPlayer;
 import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageEvent;
@@ -22,14 +23,18 @@ import org.bukkit.event.player.PlayerChangedWorldEvent;
 public class GameModeListeners extends ConstructorRegisterListener {
     @EventHandler
     public void onPlayerChangedWorld(PlayerChangedWorldEvent e) {
-        String world = e.getPlayer().getWorld().getName();
+        VLPlayer player = VLPlayer.getPlayer(e.getPlayer());
+        String world = player.getWorld().getName().toLowerCase();
 
-        if (world.contains("clans") || world.contains("Survival") || world.toLowerCase().contains("skyblock")) {
-            e.getPlayer().setGameMode(GameMode.SURVIVAL);
-        } else if (world.contains("Lobby")) {
-            e.getPlayer().setGameMode(GameMode.ADVENTURE);
-        } else if (world.contains("Creative")) {
-            e.getPlayer().setGameMode(GameMode.CREATIVE);
+        if (world.contains("clans") || world.contains("survival") || world.toLowerCase().contains("skyblock")) {
+            player.setGameMode(GameMode.SURVIVAL);
+        } else if (world.contains("lobby") || world.contains("kingdoms")) {
+            player.setGameMode(GameMode.ADVENTURE);
+        } else if (world.contains("creative")) {
+            player.setGameMode(GameMode.CREATIVE);
+        } else if (world.contains("build")) {
+            player.setGameMode(GameMode.CREATIVE);
+            player.sendMessageByKey("vaultcore.build.notification");
         }
     }
 
