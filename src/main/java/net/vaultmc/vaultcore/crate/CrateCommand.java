cf6875ca003309db9e30545f594e4e5fd65df0e3
yangyang200 <lin_dl@126.com>
2020-06-23 15:18:17 +0800

    Make /crate console-friendly

--- src/main/java/net/vaultmc/vaultcore/crate/CrateCommand.java
+++ src/main/java/net/vaultmc/vaultcore/crate/CrateCommand.java
@@ -18,6 +18,7 @@ import net.vaultmc.vaultcore.Permissions;
 import net.vaultmc.vaultcore.VaultCore;
 import net.vaultmc.vaultloader.utils.ItemStackBuilder;
 import net.vaultmc.vaultloader.utils.commands.*;
+import net.vaultmc.vaultloader.utils.player.VLCommandSender;
 import net.vaultmc.vaultloader.utils.player.VLPlayer;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -33,7 +34,6 @@ import java.util.stream.Collectors;
         description = "Manage crates."
 )
 @Permission(Permissions.ManageCrates)
-@PlayerOnly
 public class CrateCommand extends CommandExecutor {
     @Getter
     private static final List<Location> accessPoints;
@@ -108,49 +108,49 @@ public class CrateCommand extends CommandExecutor {
     }
 
     @SubCommand("key")
-    public void key(VLPlayer player, Collection<VLPlayer> players) {
+    public void key(VLCommandSender sender, Collection<VLPlayer> players) {
         for (VLPlayer p : players) {
             p.getDataConfig().set("crate-keys", p.getDataConfig().getInt("crate-keys", 0) + 1);
             p.saveData();
             if (p.getWorld().getName().equalsIgnoreCase("lobby")) {
                 p.getInventory().setItem(2, new ItemStackBuilder(Material.TRIPWIRE_HOOK)
-                        .amount(Math.min(127, Math.max(1, player.getDataConfig().getInt("crate-keys", 0))))
-                        .name(ChatColor.GREEN + "Crate Keys " + ChatColor.YELLOW + "(" + player.getDataConfig().getInt("crate-keys", 0) + ")")
+                        .amount(Math.min(127, Math.max(1, p.getDataConfig().getInt("crate-keys", 0))))
+                        .name(ChatColor.GREEN + "Crate Keys " + ChatColor.YELLOW + "(" + p.getDataConfig().getInt("crate-keys", 0) + ")")
                         .build());
             }
-            p.sendMessageByKey("vaultcore.commands.crate.obtained-key", "player", player.getFormattedName());
+            p.sendMessageByKey("vaultcore.commands.crate.obtained-key", "player", sender.getFormattedName());
         }
-        player.sendMessageByKey("vaultcore.commands.crate.given-key", "players", String.join(", ", players.stream().map(VLPlayer::getFormattedName).collect(Collectors.toList())));
+        sender.sendMessageByKey("vaultcore.commands.crate.given-key", "players", String.join(", ", players.stream().map(VLPlayer::getFormattedName).collect(Collectors.toList())));
     }
 
     @SubCommand("list")
-    public void list(VLPlayer player) {
-        items.forEach((item, weight) -> player.sendMessageByKey("vaultcore.commands.crate.list-item", "itemType", item.getI18NDisplayName(),
+    public void list(VLCommandSender sender) {
+        items.forEach((item, weight) -> sender.sendMessageByKey("vaultcore.commands.crate.list-item", "itemType", item.getI18NDisplayName(),
                 "weight", String.valueOf(weight), "hash", String.valueOf(item.hashCode())));
     }
 
     @SubCommand("listAccessPoint")
-    public void listAccessPoint(VLPlayer player) {
-        accessPoints.forEach(loc -> player.sendMessageByKey("vaultcore.commands.crate.list-access-point", "w", loc.getWorld().getName(),
+    public void listAccessPoint(VLCommandSender sender) {
+        accessPoints.forEach(loc -> sender.sendMessageByKey("vaultcore.commands.crate.list-access-point", "w", loc.getWorld().getName(),
                 "x", String.valueOf(loc.getBlockX()), "y", String.valueOf(loc.getBlockY()), "z", String.valueOf(loc.getBlockZ())));
     }
 
     @SubCommand("add")
-    public void add(VLPlayer player, ItemStack item, int weight) {
+    public void add(VLCommandSender sender, ItemStack item, int weight) {
         items.put(item, weight);
         itemsList.add(item);
-        player.sendMessageByKey("vaultcore.commands.crate.added-item");
+        sender.sendMessageByKey("vaultcore.commands.crate.added-item");
     }
 
     @SubCommand("addAccessPoint")
-    public void addAccessPoint(VLPlayer player, World world, Location loc) {
+    public void addAccessPoint(VLCommandSender sender, World world, Location loc) {
         loc.setWorld(world);
         accessPoints.add(loc);
-        player.sendMessageByKey("vaultcore.commands.crate.created-access-point");
+        sender.sendMessageByKey("vaultcore.commands.crate.created-access-point");
     }
 
     @SubCommand("remove")
-    public void remove(VLPlayer player, int hash) {
+    public void remove(VLCommandSender sender, int hash) {
         ItemStack item = null;
         for (ItemStack i : items.keySet()) {
             if (i.hashCode() == hash) {
@@ -159,13 +159,20 @@ public class CrateCommand extends CommandExecutor {
             }
         }
         items.remove(item);
-        player.sendMessageByKey("vaultcore.commands.crate.removed-item");
+        sender.sendMessageByKey("vaultcore.commands.crate.removed-item");
     }
 
     @SubCommand("removeAccessPoint")
-    public void removeAccessPoint(VLPlayer player, World world, Location loc) {
+    public void removeAccessPoint(VLCommandSender sender, World world, Location loc) {
         loc.setWorld(world);
-        accessPoints.remove(loc);
-        player.sendMessageByKey("vaultcore.commands.crate.removed-access-point");
+        Location toRemove = null;
+        for (Location l : accessPoints) {
+            if (l.getBlockX() == loc.getBlockX() && l.getBlockY() == loc.getBlockY() && l.getBlockZ() == loc.getBlockZ() && l.getWorld().getName().equalsIgnoreCase(loc.getWorld().getName())) {
+                toRemove = l;
+                break;
+            }
+        }
+        accessPoints.remove(toRemove);
+        sender.sendMessageByKey("vaultcore.commands.crate.removed-access-point");
     }
 }
